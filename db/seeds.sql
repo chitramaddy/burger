af239fac4aabe1de831ca44ede5cd207141bda7e
Your Name <you@example.com>
2018-05-06 12:48:42 -0400

    database set up and my sql connection

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+    VALUES("Veggie Cheese Burger", true);
+INSERT INTO burgers(burger_name, devoured)
+    VALUES("Bacon Egg and Cheese Burger", false);
+INSERT INTO burgers(burger_name, devoured)
+    VALUES("The Indulgent BBQ", false);
+INSERT INTO burgers(burger_name, devoured)
+    VALUES("Mushroom Swiss Burger", false);
